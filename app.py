dc5402f096371809303bea21680d0445f6115c83
Junseok Lee <behindstarter42@gmail.com>
2024-12-31 16:59:19 +0900

    Update app.py

--- app.py
+++ app.py
@@ -35,4 +35,5 @@ def save_file():
     return "Error: Filename is required."
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
